b8d9bfaf068ab2a657f67a02fffdf7be65c2311e
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2025-04-26 15:42:30 -0500

    Bootstrap standard y footer

---
+++ Proyecto/Migrations/20250426195354_RenameTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Estudiante = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Materia = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Calificacion = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    FechaEvaluacion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grades");
+        }
+    }
+}
